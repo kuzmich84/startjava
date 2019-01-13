f21fb911a121301e4954b643c88805c65be47f49
Dmitry Kuchukov <kudim@inbox.ru>
2019-01-13 18:54:43 +0300

    Add Calculator.java

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator{
+
+	public static void main(String [] args){
+	  	String sign="%";
+	  	int a=12,b=3;
+	  	int stepen=1;
+	  	int modul2;
+
+	  	if (sign=="+"){
+	  		int summa=a+b;
+	  		System.out.println("Summa = " +summa);
+			}
+			 else if(sign=="^"){
+				for(int i=1;i<=b;i++){
+					stepen=stepen*a;
+					
+
+				}
+				System.out.println(stepen);
+			}
+				else if(sign=="%"){
+						modul2=a%b;
+						System.out.println(modul2);
+
+				} 
+	}
+}
