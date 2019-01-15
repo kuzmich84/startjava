fd6deb8ee1139019abe3f7e2c649df2c749752ef
Dmitry Kuchukov <kudim@inbox.ru>
2019-01-15 19:16:20 +0300

    Add file Unicode.java in folder Lesson_1

---
+++ src/com/startjava/Lesson_1/Unicode/Unicode.java
@@ -0,0 +1,14 @@
+package com.startjava.lesson_1.unicode;
+
+public class Unicode{
+
+	public static void main(String[] args){
+		for (char c=33;c<126;c++){
+			System.out.print(c);
+
+		}
+
+
+	}
+
+}
